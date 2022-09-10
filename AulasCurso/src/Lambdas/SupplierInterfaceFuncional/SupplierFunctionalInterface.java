22d3e7fdf678d17010833bf2b76c6594eab3e8ef
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-10 13:59:48 -0300

    Utilizando a Interface Funcional Supplier, não recebe nenhum parametro e retorna algo

---
+++ AulasCurso/src/Lambdas/SupplierInterfaceFuncional/SupplierFunctionalInterface.java
@@ -0,0 +1,15 @@
+package Lambdas.SupplierInterfaceFuncional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class SupplierFunctionalInterface {
+    public static void main(String[] args) {
+        
+        Supplier <List<String>> umLista = () -> Arrays.asList("Ana", "Bia", "Lia", "Gui");
+    
+        System.out.println(umLista.get());
+        
+    }
+}
