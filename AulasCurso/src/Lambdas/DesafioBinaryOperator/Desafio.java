4d82c23a03db11180ece2b763d9e257832b97bfd
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-10 15:06:53 -0300

    Desafio Utilizando Functional Interfaces

---
+++ AulasCurso/src/Lambdas/DesafioBinaryOperator/Desafio.java
@@ -0,0 +1,26 @@
+package Lambdas.DesafioBinaryOperator;
+
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+import javax.imageio.event.IIOReadUpdateListener;
+
+public class Desafio {
+    public static void main(String[] args) {
+        
+        Produto p = new Produto("iPad", 3235.89, 0.13);
+
+        Function <Produto, Double> precoComDesconto = num -> num.preco * (1 - num.desconto);
+        Function <Double, Double> calculoImposto = num -> num >= 2500.00 ? num + (num * 0.085) : num;
+        Function <Double, Double> frete = num -> num >= 3000.00 ? num + 100.0 : num + 50.0;
+        Function <Double, Double> casasDecimais = num -> (Math.round(num*100.0)/100.0);
+        Function <Double, String> formataPreco = num -> "Preco final: R$" + num;
+
+
+        System.out.println(precoComDesconto.apply(p));
+        System.out.println(precoComDesconto.andThen(calculoImposto).apply(p));
+        System.out.println(precoComDesconto.andThen(calculoImposto).andThen(frete).apply(p));
+        System.out.println(precoComDesconto.andThen(calculoImposto).andThen(frete).andThen(casasDecimais).apply(p));
+        System.out.println(precoComDesconto.andThen(calculoImposto).andThen(frete).andThen(casasDecimais).andThen(formataPreco).apply(p));
+    }
+}
---
+++ AulasCurso/src/Lambdas/DesafioBinaryOperator/Produto.java
@@ -0,0 +1,13 @@
+package Lambdas.DesafioBinaryOperator;
+
+public class Produto {
+    String nome;
+    Double preco;
+    Double desconto;
+
+    Produto(String nome, Double preco, Double desconto){
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
