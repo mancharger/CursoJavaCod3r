53e2a3cf786ad9248e0a2a33a2174f70e5d3cab5
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-12 16:54:26 -0300

    Primeiro uso de Streams

---
+++ AulasCurso/src/StreamsAPI/Testestream.java
@@ -0,0 +1,15 @@
+package StreamsAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Testestream {
+    public static void main(String[] args) {
+        
+        List<String> listanomes = Arrays.asList("Franklin", "Eduardo", "Moreira", "Santos");
+        
+        Stream<String> streamNomes = listanomes.stream();
+        streamNomes.forEach(System.out::println);
+    }
+}
