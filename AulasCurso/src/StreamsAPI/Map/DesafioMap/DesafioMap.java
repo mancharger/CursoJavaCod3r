b38a3ae163a4f92fab91e02f35be15a809b59ff2
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-14 17:57:09 -0300

    Desafio Maps

---
+++ AulasCurso/src/StreamsAPI/Map/DesafioMap/DesafioMap.java
@@ -0,0 +1,31 @@
+package StreamsAPI.Map.DesafioMap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+public class DesafioMap {
+    public static void main(String[] args) {
+        
+        List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        // Function<Integer, String> paraBinario = m -> m.toBinaryString(m);
+        Function<String, String> inverterBinario = m -> {
+            String invertido = new StringBuilder(m).reverse().toString();
+            return invertido;            
+        };
+        Function<String, Integer> paraDecimal = m -> {
+            Integer numInteiro = Integer.parseUnsignedInt(m, 2);
+            return numInteiro;
+        };
+
+
+        nums.stream()
+            .map(Integer::toBinaryString)
+            .map(inverterBinario)
+            .map(paraDecimal)
+            .forEach(System.out::println);
+    }
+}
