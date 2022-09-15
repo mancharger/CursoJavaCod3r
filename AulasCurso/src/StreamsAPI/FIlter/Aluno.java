fbe5fd0fadb00e7cde9db3555925501d88c46530
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-15 12:55:11 -0300

    Filter - StreamAPI

---
+++ AulasCurso/src/StreamsAPI/FIlter/Aluno.java
@@ -0,0 +1,11 @@
+package StreamsAPI.FIlter;
+
+public class Aluno {
+    final String nome;
+    final Double nota;
+    
+    public Aluno(String nome, Double nota){
+        this.nome = nome;
+        this.nota = nota;
+    }
+}
---
+++ AulasCurso/src/StreamsAPI/FIlter/Filter.java
@@ -0,0 +1,26 @@
+package StreamsAPI.FIlter;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Filter {
+    public static void main(String[] args) {
+        
+        Aluno a1 = new Aluno("Ana", 7.8); 
+        Aluno a2 = new Aluno("Bia", 5.8); 
+        Aluno a3 = new Aluno("Daniel", 9.8); 
+        Aluno a4 = new Aluno("Gui", 6.8); 
+        Aluno a5 = new Aluno("Rebeca", 7.1); 
+        Aluno a6 = new Aluno("Pedro", 8.8);
+        
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6);
+
+        Predicate<Aluno> aprovado = a -> a.nota >= 7;
+
+        alunos.stream()
+            .filter(aprovado)
+            .map(a -> "Parabéns! " + a.nome + "! Voce foi aprovado(a)!")
+            .forEach(System.out::println);
+    }
+}
