c36f3edf8e0230494062dde4c8a28a7d528de446
Franklin Eduardo <fhcdudu13@gmail.com>
2022-09-17 20:28:03 -0300

    Mais informacoes sobre reduce

---
+++ AulasCurso/src/StreamsAPI/Reduce/Reduce3.java
@@ -0,0 +1,34 @@
+package StreamsAPI.Reduce;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+import StreamsAPI.FIlter.Filter;
+
+public class Reduce3 {
+    public static void main(String[] args) {
+        
+        Aluno a1 = new Aluno("Ana", 7.1);
+        Aluno a2 = new Aluno("Luna", 6.1);
+        Aluno a3 = new Aluno("Gui", 8.1);
+        Aluno a4 = new Aluno("Gabi", 10.0);
+
+        List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+        Predicate<Aluno> aprovado = a -> a.nota >= 7;
+        Function<Aluno, Double> apenasNota = a -> a.nota;
+        BiFunction<Media, Double, Media> calcularMedia = (media, nota) -> media.adicionar(nota);
+        BinaryOperator<Media> combinarMedia = (m1, m2) -> Media.combinar(m1, m2);
+
+        Media media = alunos.stream()
+            .filter(aprovado)
+            .map(apenasNota)
+            .reduce(new Media(), calcularMedia, combinarMedia);
+
+        System.out.println("A media dos aprovados eh: " + media.getValor());
+    }
+}
