b19d2dc7dc11e98e0b6f79d6edf3572b949bbe3c
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-06 11:42:29 -0300

    Excecoes como causa / questoes de causalidade e como tratá-las

---
+++ AulasCurso/src/Excecao/Causa.java
@@ -0,0 +1,30 @@
+package Excecao;
+
+public class Causa {
+    public static void main(String[] args) {
+        
+        try {
+            metodoA(null);
+        } catch (IllegalArgumentException e) {
+            if (e.getCause() != null) {
+                System.out.println(e.getCause().getMessage());
+            }
+        }
+    }   
+    
+    static void metodoA(Aluno aluno){
+        try {
+            metodoB(aluno);
+        } catch (Exception causa) {
+            throw new IllegalArgumentException(causa);
+        }
+    }
+
+    static void metodoB(Aluno aluno){
+        if (aluno == aluno) {
+            throw new NullPointerException("Aluno está Nulo!");
+        }
+        System.out.println(aluno.nome);
+    }
+
+}
