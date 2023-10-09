dc2fdbbf671119c534ced7d75e4452cdaa4a35c3
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-09 12:25:56 -0300

    primeiro teste / configuração de ambiente de testes

---
+++ CampoMinadoProject/test/br/com/frank/cm/Teste.java
@@ -0,0 +1,20 @@
+package br.com.frank.cm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+public class Teste {
+
+    @Test
+    public void testando(){
+        int a = 1 + 1;
+
+        assertEquals(2, a);
+    }
+
+    
+
+}
