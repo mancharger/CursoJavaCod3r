ef5a65f7d33b16c2a76e34764c7db185b9fb87ed
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-18 14:56:51 -0300

    finalizacao classe campo

--- CampoMinadoProject/src/br/com/frank/cm/modelo/Campo.java
+++ CampoMinadoProject/src/br/com/frank/cm/modelo/Campo.java
@@ -87,4 +87,45 @@ public class Campo {
     public boolean isFechado(){
         return !isAberto();
     }
+
+    public int getLinha(){
+        return linha;
+    }
+
+    public int getColuna(){
+        return coluna;
+    }
+
+    boolean objetivoAlcancado(){
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+        return desvendado || protegido;
+    }
+    
+    long minasNaVizinhanca(){
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar(){
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+
+    public String toString(){
+        if (marcado) {
+            return "x";
+        } else if (aberto && minado){
+            return "*";
+        } else if (aberto && minasNaVizinhanca() > 0){
+            return Long.toString(minasNaVizinhanca());
+        } else if (aberto){
+            return " ";
+        } else {
+            return "?";
+        }
+
+        
+    }
+
 }
