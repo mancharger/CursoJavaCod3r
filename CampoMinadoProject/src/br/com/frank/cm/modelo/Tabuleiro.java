4f3f3705772b83224bb1a4a190379e5db9e1a556
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-19 15:19:52 -0300

    iniciando classe tabuleiro

--- CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
+++ CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
@@ -1,5 +1,21 @@
 package br.com.frank.cm.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tabuleiro {
     
+    private int linhas;
+    private int colunas;
+    private int minas;
+
+    private final List<Campo> campos = new ArrayList<>();
+
+    public Tabuleiro(int linhas, int colunas, int minas){
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.minas = minas;
+    }
+
+
 }
