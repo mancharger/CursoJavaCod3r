30941a4bda43ca1d897fc5eb95e5efb417c3ea86
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-19 16:05:24 -0300

    desenvolvendo o tabuleiro

--- CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
+++ CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
@@ -2,6 +2,7 @@ package br.com.frank.cm.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
     
@@ -15,7 +16,42 @@ public class Tabuleiro {
         this.linhas = linhas;
         this.colunas = colunas;
         this.minas = minas;
+
+        gerarCampos();
+        associarVizinhos();
+        sortearMinas();
     }
+
+    private void gerarCampos() {
+        for (int l = 0; l < linhas; l++) {
+            for (int c = 0; c < colunas; c++) {
+                campos.add(new Campo(l, c));
+            }
+        }
+    }
+
+    
+    private void associarVizinhos() {
+        for(Campo c1: campos){
+            for(Campo c2: campos){
+                c1.adicionarVizinho(c2);
+            }
+        }
+    }
+    
+    private void sortearMinas() {
+        long minasArmadas = 0;
+
+        Predicate<Campo> minado = c -> c.isMinado();
+
+        do {
+            minasArmadas = campos.stream().filter(minado).count();
+            int aleatorio = (int) (Math.random() * campos.size());
+            campos.get(aleatorio).minar();
+        } while (minasArmadas < minas);
+
+    }
+
 
 
 }
