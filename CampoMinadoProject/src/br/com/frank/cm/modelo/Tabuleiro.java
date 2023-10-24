8e3936b4c97fb98e4550ff48984d22ae125cbe13
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-24 15:57:01 -0300

    correcao quantidade de minas

--- CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
+++ CampoMinadoProject/src/br/com/frank/cm/modelo/Tabuleiro.java
@@ -65,9 +65,9 @@ public class Tabuleiro {
         Predicate<Campo> minado = c -> c.isMinado();
 
         do {
-            minasArmadas = campos.stream().filter(minado).count();
             int aleatorio = (int) (Math.random() * campos.size());
             campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
         } while (minasArmadas < minas);
 
     }
