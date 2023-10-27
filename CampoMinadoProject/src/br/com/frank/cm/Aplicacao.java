c28ee73f773b37123fdce471d4416ea4673d3052
Franklin Eduardo <fhcdudu13@gmail.com>
2023-10-27 20:53:18 -0300

    testes

--- CampoMinadoProject/src/br/com/frank/cm/Aplicacao.java
+++ CampoMinadoProject/src/br/com/frank/cm/Aplicacao.java
@@ -6,6 +6,7 @@ import br.com.frank.cm.visao.TabuleiroConsole;
 public class Aplicacao {
     public static void main(String[] args) {
         //Projeto aos moldes da organização MVC
+        //Main - Iniciação projeto
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
         new TabuleiroConsole(tabuleiro);
 
